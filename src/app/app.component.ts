5c21f3325145288d75b45bdc18fcd931e30ddf4a
AndresR2909 <felipeandres29@hotmail.com>
2022-10-31 19:12:55 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'proyecto_WEB_misiontic_2022_C4_G30_E8';
+}
